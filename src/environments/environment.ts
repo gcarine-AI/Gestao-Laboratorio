10f099a3a652a00ddf358316f939ae27bbfa139c
Carine Gonçalves <carinegoncalves@mbp-de-carine.home>
2026-03-18 19:20:27 +0000

    supabase correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseURL: '',
-  supabaseKey: ''
+  supabaseURL: 'https://eihiptuwbzcoapufspzi.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpaGlwdHV3Ynpjb2FwdWZzcHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NjA3OTcsImV4cCI6MjA4OTIzNjc5N30.AFG-dtC52YbASDGaYr90nPx9_p_S4EAB7RAArKiQox4'
 }
