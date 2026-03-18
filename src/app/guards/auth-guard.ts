3b1d52673f639dcbed01b88a4e1d428d0224c597
Carine Gonçalves <carinegoncalves@MacBook-Pro-de-Carine.local>
2026-03-18 09:21:18 +0000

    fix: fix unused vars in auth guard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -2,16 +2,16 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { Supabase } from '../services/supabase';
 
-export const authGuard: CanActivateFn = async (route, state) => {
+export const authGuard: CanActivateFn = async () => {
   const supabase = inject(Supabase);
   const router = inject(Router);
 
   const { data } = await supabase.getClient().auth.getUser();
 
   if (data.user) {
-    return true; // utilizador autenticado, deixa entrar
+    return true;
   } else {
-    router.navigate(['/auth']); // redireciona para login
+    router.navigate(['/auth']);
     return false;
   }
 };
